9b5a87017ccbfb9ccc57e26ef61ddf7de10923c7
Ashish <akarhade5@gmail.com>
2020-05-27 09:07:55 +0530

    DAY 10

--- coding_blocks/algo_c++/arrays/challenges/string_camel_case.py
+++ coding_blocks/algo_c++/arrays/challenges/string_camel_case.py
@@ -1,9 +1,13 @@
-import re 
-
 def camel_case_split(str): 
-	return re.findall(r'[A-Z](?:[a-z]+|[A-Z]*(?=[A-Z]|$))', str) 
+      
+    start_idx = [i for i, e in enumerate(str) 
+                 if e.isupper()] + [len(str)] 
+  
+    start_idx = [0] + start_idx 
+    return [str[x: y] for x, y in zip(start_idx, start_idx[1:])]
 	
 str = input()
 ans = camel_case_split(str)
 for a in ans:
 	print(a)
+
