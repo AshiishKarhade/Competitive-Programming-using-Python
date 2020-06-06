a415cf57a6693ca57cc439231fd852ec821b0a77
Ashish <akarhade5@gmail.com>
2020-06-06 13:25:58 +0530

    DAY 15

---
+++ coding_blocks/algo_c++/stacks/stacks.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Stack{
+    private:
+        vector <int> v;
+    public:
+        void push(int data){
+            v.push_back(data);
+        }
+
+        bool empty(){
+            return v.size()==0;
+        }
+
+        void pop(){
+            if(!empty()){
+                v.pop_back();   
+            }
+        }
+
+        int top(){
+                return v[v.size()-1]
+        }
+};
+
+int main(){
+
+    Stack s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    
+    
+    
+    return 0;
+}
