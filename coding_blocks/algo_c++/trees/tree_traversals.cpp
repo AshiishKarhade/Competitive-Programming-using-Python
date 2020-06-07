2a370784b5c8830d07b5f432976bc0c8eec9117e
Ashish <akarhade5@gmail.com>
2020-06-07 13:36:08 +0530

    DAY 16

--- coding_blocks/algo_c++/trees/tree_traversals.cpp
+++ coding_blocks/algo_c++/trees/tree_traversals.cpp
@@ -15,19 +15,44 @@ class node{
 };
 
 node* build_tree(){
+    int data;
+    cin >> data;
 
+    if(data==-1){
+        return NULL;
+    }
+    node* root = new node(data);
+    root->left = build_tree();
+    root->right = build_tree();
+
+    return root;
 }
 
 void inorder_traversal(node* root){
-
+    if(root == NULL){
+        return;
+    }
+    inorder_traversal(root->left);
+    cout << root->data << " ";
+    inorder_traversal(root->right);
 }
 
 void preOrder_traversal(node* root){
-
+    if(root==NULL){
+        return;
+    }
+    cout << root->data << " ";
+    preOrder_traversal(root->left);
+    preOrder_traversal(root->right);
 }
 
 void postOrder_traversal(node* root){
-
+    if(root==NULL){
+        return;
+    }
+    postOrder_traversal(root->left);
+    postOrder_traversal(root->right);
+    cout << root->data << " ";
 }
 
 int main(){
@@ -35,14 +60,17 @@ int main(){
     node* root = build_tree();
     
     //INORDER
+    cout << "INORDER: ";
     inorder_traversal(root);
     cout << "\n";
 
     //PREORDER
+    cout << "PREORDER: ";
     preOrder_traversal(root);
     cout << "\n";
-    
+
     //POSTORDER
+    cout << "POSTORDER: ";
     postOrder_traversal(root);
     
     return 0;
